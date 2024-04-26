29b0c7bc26bafaa1e8fd9ee96e011900d778d739
MarekEffenberger <marekeff@seznam.cz>
2024-04-26 18:25:30 +0200

    DID YOU EVER PLAY RUGBY?

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_57626.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_57626.dir/CMakeCCompilerABI.c.o"
+  "cmTC_57626"
+  "cmTC_57626.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_57626.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
